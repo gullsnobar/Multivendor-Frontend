85da59f6673208ee3b7f59c7d14a865e77689aa0
gullsnobar <abdulrehman7775@gmail.com>
2026-06-10 09:25:49 +0500

    fixed issues in server.js file

--- src/server.js
+++ src/server.js
@@ -1,2 +1,2 @@
-export const server = "/api/v2";
-export const backend_url = ""; 
+export const server = "https://multivendor-backend-pi.vercel.app/api/v2";
+export const backend_url = "https://multivendor-backend-pi.vercel.app";
